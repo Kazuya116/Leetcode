f13b3652a61eae79dc600ad791ab995f0e0852d2
Kazuya116 <amanpratapsingh116@gmail.com>
2024-02-11 22:01:40 +0530

    version control

--- ama.java
+++ ama.java
@@ -1,6 +1,7 @@
 public class ama {
     public static void main(String[] args) {
         System.out.println("hello aman to github");
+        System.out.println("okay let say hello");
     }
     
 }
